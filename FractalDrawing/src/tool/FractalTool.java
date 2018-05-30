7f83d13a3622ec9b6bd8bbdee06f7a5bb45947df
Nick Baijens <nbnickbaijens@gmail.com>
2018-05-30 16:25:29 +0200

    Some refactoring

--- FractalDrawing/src/tool/FractalTool.java
+++ FractalDrawing/src/tool/FractalTool.java
@@ -1,10 +1,21 @@
 package tool;
 
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class FractalTool {
+	
+	static Scanner myInput = new Scanner(System.in);
 	private Integer angle;
 	private Integer newAngle;
+	
+	public FractalTool() {
+		System.out.println("Draw a line");
+		System.out.println("For each new angle shown, draw a new line from your previous line at the angle shown");
+		System.out.println("If you hit the edge of the paper or another line, draw branches with the same rules.");
+		System.out.println("Repeat till your page is filled");
+		inputHandler();
+	}
 	
 	public Integer getAngle() {
 		
@@ -17,5 +28,23 @@ public class FractalTool {
 			newAngle = ThreadLocalRandom.current().nextInt(-25, 25);
 		}
 		angle = newAngle;
+	}
+	
+	public void inputHandler() {
+		while(true) {
+			System.out.println("press enter for new angle or q to quit");
+			String antwoord = myInput.nextLine();
+			if(antwoord.equals("")) {
+				setAngle();
+				angle = getAngle();
+				System.out.println(angle);
+			}
+			else if(antwoord.equals("q")) {
+				System.exit(0);
+			}
+			else {
+				System.out.println("unknown command");
+			}
+		}
 	}
 }
